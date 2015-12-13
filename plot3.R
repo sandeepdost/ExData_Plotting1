407b435b55f6adf745a0c70c25daa766ac11df68
Sandeep Agrawal <sagrawal@iii.com>
2015-12-13 12:01:22 +0530

    Adding code and png charts as produced

---
+++ plot3.R
@@ -0,0 +1,21 @@
+DrawPlot3 <- function() {
+  file <- "household_power_consumption.txt"
+	powerData <- read.table(file, header=T, sep=";")
+	powerData$Date <- as.Date(powerData$Date, format="%d/%m/%Y")
+	df <- powerData[(powerData$Date=="2007-02-01") | (powerData$Date=="2007-02-02"),]
+	df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+	df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+	df$Voltage <- as.numeric(as.character(df$Voltage))
+	df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+	df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+	df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+	df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+	
+	plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+	lines(df$timestamp,df$Sub_metering_2,col="red")
+	lines(df$timestamp,df$Sub_metering_3,col="blue")
+	legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+	dev.copy(png, file="plot3.png", width=480, height=480)
+	dev.off()
+	cat("plot3.png has been saved in", getwd())
+}
